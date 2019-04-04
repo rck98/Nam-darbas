bc60d947097032c24764058f21e1f080ce5ad8ab
rck98 <48723597+rck98@users.noreply.github.com>
2019-04-04 21:06:00 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,95 +1,66 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
-#include <string>
 #include <vector>
-#include <stdio.h>
 #include <algorithm>
-
+#include <iomanip>
+#include <string.h>
 using namespace std;
-
+struct ir {
+string var, pav;
+vector<int>nd;
+double egz, vid, med;
+};
 int main()
 {
-    string vardas[15], pavarde[15];
-    int a=1;
-    double galutinis[15];
-    double egzaminas;
-    double mediana[15];
-    double *pazymiai, *temp;
-    double k;
-    int p;
-    int x=0;
+    vector<ir>lent;
+    ir temp;
+    int temp_paz=1,  temp_a=1, temp_b, temp_sum;
+    double temp_vid1, temp_med1, temp_med2;
+    while(temp_a==1)
+    {
+        temp_paz=1;
+        cout<<"Iveskite Varda ir Pavarde ";
+        cin>>temp.var>>temp.pav;
+        temp.nd.clear();
+        temp_sum=0;
+        cout<<"Suveskite pazymius norint sustoti iveskite 0: ";
+        while (temp_paz!=0 && temp_paz>0 && temp_paz<11)
+               {
+                   cin>>temp_paz;
+                   temp.nd.push_back(temp_paz);
+                   temp_sum+=temp_paz;
+               }
+               temp_vid1=temp_sum*1.0/(temp.nd.size()-1);
+               cout<<"Iveskite egzamino rezultata: ";
+               cin>>temp.egz;
+               temp.nd.push_back(temp.egz);
+               temp.vid=temp_vid1*0.4+temp.egz*0.6;
+               sort (temp.nd.begin(),temp.nd.end());
+               temp.nd.erase(temp.nd.end()-1);
+               if(temp.nd.size()%2==0)
+               {
+                temp_med1=temp.nd.size()/2;
+                temp_med2=temp.nd.size()/2+1;
+                temp.med=(temp.nd[temp_med1]+temp.nd[temp_med2])*1.0/2;
+               }
+                else
+                    {
+                    temp_med1=temp.nd.size()/2+1;
+                    temp.med=temp.nd[temp_med1];
+                    }
+                    cout<<"Jei norite testi spauskite 1 ";
+                    cin>>temp_a;
+                                }
+                    cout<<"Jei norite, kad rodytu vidurki spauskite 1, jei mediana spauskite 2 ";
+                    cin>>temp_b;
+                    if(temp_b==1)
+                    {
+                    cout<<setw(15)<<left<<"Vardas "<<setw(15)<<left<<"Pavarde "<<setw(20)<<right<<"Galutinis (Vid.) "<<endl;
+                    cout.fill('-');
+                    cout.width(50);
+                    cout<<"-"<<endl;
+                    cout.fill(' ');
+                    for(int i=0; i<temp.var.size();i++)
+                    cout<<setw(15)<<left<<temp.var<<setw(15)<<left<<temp.pav<<setw(20)<<right<<temp.med<<endl;
+                    }
 
-    do
-    {
-        x++;
-        double sum=0;
-    int n=1;
-    pazymiai = new double [n];
-    cout<<"iveskite Varda: ";
-    cin>>vardas[x];
-    cout<<"iveskite Pavarde: ";
-    cin>>pavarde[x];
-    cout<<"Iveskite namu darbu rezultatus: " ;
-    while(k!=0){
-        cin>>k;
-    if (k!=0 && k>0 && k<11)
-    {
-        pazymiai[n]=k;
-        temp=new double [n+1];
-        temp=pazymiai;
-        delete[](pazymiai);
-        n++;
-        pazymiai=new double [n];
-        pazymiai=temp;
-        delete[](temp);
-    }
-    }
-    k=1;
-    for(int i=0 ;i<n ;i++)
-    {
-        sum+=pazymiai[i];
-    }
-        cout<<"Iveskite egzamino rezulta: ";
-    cin>>egzaminas;
-    pazymiai[n]= egzaminas;
-    galutinis[x]=(sum/(n-1))*0.4+0.6*egzaminas;
-    sort(pazymiai,pazymiai+n+1);
-    if(n%2!=0)
-    {
-        p=(n/2)+1;
-     mediana[x]=pazymiai[p];
-    }
-    else
-    {
-        p=n/2;
-        mediana[x]=(pazymiai[p]+pazymiai[p+1])/2;
-    }
-    cout<<"Jei norite testi spauskite 1 ";
-        cin>>a;
-}
-        while (a==1);
-        cout<<"Spauskite 1 noredami pamatyti vidurki, spauskite 2 noredami pamatyti mediana: ";
-        int b;
-        cin>>b;
-    if(b==1)
-    {
-    cout<<setw(15)<<left<<"Vardas "<<setw(15)<<left<<"Pavarde "<<setw(20)<<right<<"Galutinis (Vid.) "<<endl;
-    cout.fill('-');
-    cout.width(50);
-    cout<<"-"<<endl;
-    cout.fill(' ');
-    for(int j=1;j<=x;j++)
-    cout<<setw(15)<<left<<vardas[j]<<setw(15)<<left<<pavarde[j]<<setw(20)<<right<<setprecision(3)<<right<<galutinis[j]<<endl;
-    }
-    else
-    {
-        cout<<setw(15)<<left<<"Vardas "<<setw(15)<<left<<"Pavarde "<<setw(20)<<right<<"Galutinis (Mediana) "<<endl;
-    cout.fill('-');
-    cout.width(50);
-    cout<<"-"<<endl;
-    cout.fill(' ');
-    for(int j=1;j<=x;j++)
-    cout<<setw(15)<<left<<vardas[j]<<setw(15)<<left<<pavarde[j]<<setw(20)<<right<<setprecision(3)<<right<<mediana[j]<<endl;
-    }
 }
