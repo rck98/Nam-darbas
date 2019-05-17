a1f40865724905a608c3a58b8a109e7652009904
rck98 <48723597+rck98@users.noreply.github.com>
2019-05-17 08:51:46 +0300

    Delete PenkiFailai.h

--- PenkiFailai.h
+++
@@ -1,9 +0,0 @@
-#ifndef PENKIFAILAI_H_INCLUDED
-#define PENKIFAILAI_H_INCLUDED
-#include "mano.h"
-using namespace std;
-void PenkiFailai(vector<mokinys>&lentele)
-
-
-
-#endif // PENKIFAILAI_H_INCLUDED
